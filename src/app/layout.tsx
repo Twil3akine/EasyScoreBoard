a531637ff5a1e2b79b5a681d0d6b71439cd3d885
Twil3akine <twil3akine@gmail.com>
2024-11-18 16:27:45 +0900

    change theme component from button to toggle & refactered

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,21 @@
 import "@/global.css"
+import React, { useState } from "react"
 import Page from "@/app/app"
 
 const Layout: React.FC = () => {
+  const [currentTheme, setCurrentTheme] = useState<"light" | "dark">("light")
+
+  const toggleTheme: () => void = () => {
+    const newTheme: "light"|"dark" = currentTheme==="light" ? "dark":"light"
+    document.documentElement.setAttribute("data-theme", newTheme)
+    setCurrentTheme(newTheme)
+  }
 
   return (
-    <Page />
+    <div>
+      <Page currentTheme={currentTheme} toggleTheme={toggleTheme} />
+    </div>
   )
 }
 
-export default Layout;
+export default Layout
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,8 @@ import ReactDOM from "react-dom/client";
 import Layout from "@/app/layout"
 import "@/global.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const Main: React.FC = () => {
+	return <Layout />
+}
 
-root.render(
-	<React.StrictMode>
-		<Layout />
-	</React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<Main />)
